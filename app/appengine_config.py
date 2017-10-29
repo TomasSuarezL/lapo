7bf2c0538ecf111524251c58bd0a7db559369f35
Tomas <tomas.sl@hotmail.com>
2017-10-29 12:45:02 -0300

    Flask App for GAE innit

---
+++ app/appengine_config.py
@@ -0,0 +1,16 @@
+# [START vendor]
+from google.appengine.ext import vendor
+import os
+import sys
+
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
+# [END vendor]
+
+if os.environ.get('SERVER_SOFTWARE', '').startswith('Google App Engine'):
+    sys.path.insert(0, 'lib.zip')
+else:
+    if os.name == 'nt':
+        os.name = None
+        sys.platform = ''
+
